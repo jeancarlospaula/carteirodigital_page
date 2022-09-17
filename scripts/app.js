65a222a52446d9f96965e7aaff55b3b3d2952bde
jean.depaula <jean.depaula@obraprimaweb.com.br>
2022-09-16 23:29:12 -0300

    hotfix: Calculation year to show on footer

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,10 @@ import { scrollAnimation } from "./utils/scrollAnimation.js";
 import { toggleNavbar } from "./utils/toggleNavbar.js";
 
 const start = () => {
+  const footerYear = document.querySelector(".footer-year");
+  const currentYear = new Date().getFullYear();
+  footerYear.textContent = currentYear;
+
   toggleNavbar();
   scrollAnimation();
 };
